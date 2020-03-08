c0fce5121d3d985c9172ef7bec2fc8044d1d63bc
TomCoupe <thomas.coupe@yahoo.co.uk>
2020-03-08 13:47:16 +0000

    vue js stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import homepage from './components/homepage.vue';
+
+Vue.component('homepage', (homepage).default);
+
+const app = new Vue({
+    el: '#app'
+});
