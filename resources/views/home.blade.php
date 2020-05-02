624287adc99736f9f64c26f8b01da99b02551db0
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-05-02 16:21:02 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('main')
+<div class="header position-center">
+    <h1>Header</h1>
+</div>
+<div class="card">
+    <div class="card-body">
+        sdjfbsdkjfbksdbfksdbkflbs
+        <homepage></homepage>
+    </div>
+</div>
