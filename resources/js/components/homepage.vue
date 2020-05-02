ffe5be03e2efdb5b8573f393b0e4833c70796e8a
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-05-02 22:12:16 +0100

    started to make a champions stat page.

--- resources/js/components/homepage.vue
+++ resources/js/components/homepage.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="center-champion-search">
-        <h2 class="card-title">Champion Search</h2>
-        <input class="form-control" placeholder="Please input a champion name">
+        <h2 class="card-title fun-font">Champion Search</h2>
+        <input list="champion" class="form-control" placeholder="Please input a champion name">
+        <datalist id="champion">
+            <option v-for="champ in championArray" :value="champ.name"></option>
+        </datalist>
         <button class="btn btn-dark">Search</button>
     </div>
 </template>
 
 <script>
+    import champions from 'lol-champions';
+
 export default {
     name: 'Homepage.vue',
     data() {
         return {
-
+            championArray: champions,
         }
     },
+    mounted() {
+        console.log(champions);
+    }
 }
 </script>
